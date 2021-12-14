b0115ac4f9f73482b745369ce9610e3aeb64761f
januarsetyo <januarsn2@gmail.com>
2021-12-14 20:54:39 +0700

    commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+    <link rel="icon" href="./images/logo.png">
+    <link rel="stylesheet" href="./css/main.css">
+</head>
+<body>
+    <div class="rectangle-4"></div>
+  <a class="nav-linkk" href="/login">Login</a>
+  <h1 class="selamat-datang-di-si">SELAMAT DATANG <br />DI SISTEM PENGELOLAAN HONOR PKL</h1>
+  <img
+  class="college-2"
+  src=./images/artboard.png
+/>
+        </div>
+</body>
+</html>
